b59e1a7ec4685015ecb004ab9779fb20fb8cabae
Reynaldi Kindarto <reynaldi_kindarto@yahoo.com>
2024-07-12 12:27:43 +0700

    chore: Add route to send OTP to user

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,31 @@ const app = express()
 
 app.use(express.json())
 
+const otp_server = "http://localhost:5000"
+
 
 // TODO: Routing aplikasi akan kita tulis di sini
 
 
 // handle 404 error
-app.use((req: Request, res: Response, next: Function) => {
-  next(createError(404))
-})
+// app.use((req: Request, res: Response, next: Function) => {
+//     next(createError(404))
+// })
 
 app.listen(3000, () =>
-  console.log(`⚡️[server]: Server is running at https://localhost:3000`)
+    console.log(`⚡️[server]: Server is running at https://localhost:3000`)
 )
 
-// create hello world route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!")
+app.get("/send-otp/:username", async (req: Request, res: Response) => {
+    // hit the OTP server with username in the POST body
+    const username = req.params.username
+    const response = await fetch(`${otp_server}/send-otp`, {
+        method: "POST",
+        body: JSON.stringify({ username }),
+        headers: { "Content-Type": "application/json" },
+    })
+
+    const data = await response.json()
+
+    res.json(data)
 })
