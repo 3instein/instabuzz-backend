cad781817078643ec628881d849519d5351ee74c
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2024-07-14 21:59:59 +0700

    refactor: Add OTP controller for sending and verifying OTP
    The code changes in `otpController.ts` introduce a new controller for handling OTP (One-Time Password) functionality. The `sendOtp` function sends an OTP to a specified username using an external service. If the user already exists in the database, their OTP is updated; otherwise, a new user is created. The `verifyOtp` function verifies the OTP provided by the user and updates the user's verification status accordingly. This refactor improves code organization and separates the OTP-related logic from other controllers.

---
+++ src/controllers/otpController.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from '@prisma/client';
+import axios from 'axios';
+import { Request, Response } from 'express';
+import { generateToken } from '../jwt';
+
+const prisma = new PrismaClient();
+const IG_BOT_URL = process.env.IG_BOT_URL || "http://localhost:5000";
+
+export const sendOtp = async (req: Request, res: Response) => {
+    const { username } = req.params;
+
+    try {
+        const response = await axios.post(`${IG_BOT_URL}/send-otp`, { username });
+
+        if (response.status === 200) {
+            const otp = response.data.otp;
+
+            const existingUser = await prisma.user.findUnique({
+                where: { username }
+            });
+
+            if (existingUser) {
+                await prisma.user.update({
+                    where: { username },
+                    data: { otp }
+                });
+            } else {
+                await prisma.user.create({
+                    data: { username, otp }
+                });
+            }
+
+            return res.json({ message: "OTP sent", otp });
+        }
+
+        return res.status(response.status).json({ message: "Failed to send OTP" });
+    } catch (error: any) {
+        console.error(error);
+        return res.status(500).json({ message: "An error occurred", error: error.message });
+    }
+}
+
+export const verifyOtp = async (req: Request, res: Response) => {
+    const { username, otp } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: { username }
+    });
+
+    if (!user) {
+        return res.json({ message: "User not found" });
+    }
+
+    if (!user.otp) {
+        return res.json({ message: "OTP not sent" });
+    }
+
+    if (user.otp === otp) {
+        await prisma.user.update({
+            where: { username },
+            data: { otp: null, verified: true }
+        });
+
+        const token = generateToken(user.id, username);
+        return res.json({ message: 'OTP verified', token });
+    }
+
+    return res.json({ message: "Wrong OTP" });
+}
