c0fbbca312bc21f19bb78bce994b54298ef63206
Reynaldi Kindarto <37333204+3instein@users.noreply.github.com>
2024-07-14 19:30:52 +0700

    chore: Remove commented code

--- src/jwt.ts
+++ src/jwt.ts
@@ -11,16 +11,6 @@ export const generateToken = (id: string, username: string) => {
     return jwt.sign({ id, username }, secret, { expiresIn: '1h' });
 };
 
-// export const getUsernameFromToken = (token: string): string | null => {
-//     try {
-//         const decodedToken = jwt.verify(token, secret) as { username: string };
-//         return decodedToken.username;
-//     } catch (err) {
-//         console.error('Invalid token', err);
-//         return null;
-//     }
-// };
-
 export const decodeToken = (token: string): TokenPayload | null => {
     try {
         return jwt.verify(token, secret) as TokenPayload;
